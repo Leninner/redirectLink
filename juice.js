83e2bf5800126770f997cd390b4accd6b2f29858
Leninner <Mazabandalenin180@gmail.com>
2021-05-13 13:17:20 -0500

    Agregamos la cabecera y un logo con SVG

--- juice.js
+++ juice.js
@@ -1,3 +1,3 @@
-let usuario = prompt("¡Dime tu nombre!");
+// let usuario = prompt("¡Dime tu nombre!");
 
-alert("¡Hola! \n¡" + usuario + ", es un gusto saludarte!");
+// alert("¡Hola! \n¡" + usuario + ", es un gusto saludarte!");
